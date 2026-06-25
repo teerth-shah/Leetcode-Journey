ff95ae3d4de03c8fcf1e3b1371fd4df1d30c220c
Teerth Shah <shahteerth385@gmail.com>
2026-06-25 19:21:16 +0530

    Add comments to 69.Sqrt_of_x.java
    Added comments explaining the question and concepts learned.

--- Math/69.Sqrt_of_x.java
+++ Math/69.Sqrt_of_x.java
@@ -1,3 +1,11 @@
+## Question: 69.Srt(x) 
+
+### Concept Learned
+- Operators 
+
+### What this question taught me
+- How to square any number without using built-in function
+
 class Solution {
     public int mySqrt(int x) {
 
